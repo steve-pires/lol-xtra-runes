a682b8c19fdc546b23a436154020b12f3eb3ff98
spires <spires@wemanity.com>
2018-03-29 11:50:43 +0200

    refactoring API

--- src/app/riot-api.config.ts
+++ src/app/riot-api.config.ts
@@ -1,10 +1,12 @@
-export let api: any = {
-
-    api_key: 'RGAPI-18597450-6aa8-4054-a9e2-49d09327c5e3',
-
+const _api: any = {
     lol_static_data_v3: 'https://euw1.api.riotgames.com/lol/static-data/v3/',
+    api_key: 'RGAPI-18597450-6aa8-4054-a9e2-49d09327c5e3',
+    locale_fr: 'fr_FR'
+};
 
-    sd_v3_endpoints: {
-        runes: 'reforged-runes'
-    }
-}
+_api.sd_v3_endpoints = {
+    runes: _api.lol_static_data_v3 + 'reforged-runes/',
+    runes_paths: _api.lol_static_data_v3 + 'reforged-rune-paths/'
+};
+
+export const api = _api;
--- src/app/riot-api.service.ts
+++ src/app/riot-api.service.ts
@@ -12,15 +12,25 @@ export class RiotApiService {
   }
 
   public fetchReforgedRunes() {
-    const url: string = this.api.lol_static_data_v3 + this.api.sd_v3_endpoints.runes;
-    // const options = { 
-    //   headers: new HttpHeaders({
-    //     'api_key': this.api.api_key
-    //   })
-    // };
+    const url: string = this.api.sd_v3_endpoints.runes;
+    const params = this.getUrlParams();
+    return this.genericJsonGet(url, params);
+  }
 
-    const params = new HttpParams().set('api_key', this.api.api_key);
+  public fetchRunesPaths() {
+    const url: string = this.api.sd_v3_endpoints.runes_paths;
+    const params = this.getUrlParams();
+    return this.genericJsonGet(url, params);
+  }
 
+  private getUrlParams() {
+    const params = new HttpParams()
+      .set('locale', this.api.locale_fr)
+      .set('api_key', this.api.api_key);
+    return params;
+  }
+
+  private genericJsonGet(url: string, params: HttpParams) {
     return this._httpClient.get(url, {params: params})
       .toPromise()
       .then(response => response)
